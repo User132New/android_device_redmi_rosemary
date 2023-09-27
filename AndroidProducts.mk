36031acf72edfc335e6af37a676674ab82bd16d9
User132New <swamirahul83@gmail.com>
2023-09-27 13:30:50 +0000

    Initial Spark.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_rosemary.mk
+	$(LOCAL_DIR)/spark_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    $(foreach variant, user userdebug eng, aosp_rosemary-$(variant))
+    $(foreach variant, user userdebug eng, spark_rosemary-$(variant))
